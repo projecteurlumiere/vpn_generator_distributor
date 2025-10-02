2d40e4ba9368543929618403c69346637298dc4e
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2025-10-02 16:06:52 +0300

    add logging for group chat messages outside of admin chat

--- main.rb
+++ main.rb
@@ -21,14 +21,17 @@ return unless $PROGRAM_NAME == __FILE__
 
 Telegram::Bot::Client.run($token) do |bot|
   bot.listen do |message|
-    next if message.respond_to?(:chat) && (message.chat.type != "private" && message.chat.id != $admin_chat_id)
-
-    Thread.new do
-      Routes.instance.dispatch_controller(bot, message)
-    rescue StandardError => e
-      ApplicationController.new(bot, message).send(:reply, "Что-то пошло не так.\nЕсли вы потерялись, вернуться можно нажав на /start", reply_markup: nil)
-      LOGGER.error "Unhandled error when processing request: #{e}\n#{e.full_message}"
-      raise e
+    if message.respond_to?(:chat) && message.chat.type != "private" && message.chat.id != $admin_chat_id
+      LOGGER.error "Someone added the bot to his group chat and it was not admin chat"
+    else
+      Thread.new do
+        Routes.instance.dispatch_controller(bot, message)
+      rescue StandardError => e
+        msg = "Что-то пошло не так.\nЕсли вы потерялись, вернуться можно нажав на /start"
+        ApplicationController.new(bot, message).send(:reply, msg, reply_markup: nil)
+        LOGGER.error "Unhandled error when processing request: #{e}\n#{e.full_message}"
+        raise e
+      end
     end
   end
 end
