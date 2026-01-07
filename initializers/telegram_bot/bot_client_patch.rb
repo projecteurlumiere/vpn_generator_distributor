028518afe33ee1a9c13ca1707485950c3fbdf5b2
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2026-01-07 22:13:02 +0300

    trim fetch_update error logs

--- initializers/telegram_bot/bot_client_patch.rb
+++ initializers/telegram_bot/bot_client_patch.rb
@@ -13,10 +13,10 @@ module Telegram
         end
       rescue Faraday::TimeoutError, Faraday::ConnectionFailed => e
         # if the error of bot dying silently happens here, add method for resetting `connection` in `Api` class
-        logger.error("Faraday error: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
+        logger.error("Faraday error: #{e.class} - #{e.message}")
         retry if @running
       rescue => e
-        logger.error("Unhandled error: #{e.class} - #{e.message}\n#{e.backtrace.join("\n")}")
+        logger.error("Unhandled fetch updates error: #{e.class} - #{e.message}")
         retry if @running
       end
 
