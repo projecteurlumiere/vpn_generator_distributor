b9a45ee45aa2ac0e4b93fcac4dde71c5e554422c
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2026-02-09 23:57:16 +0300

    add install rbspy script

---
+++ scripts/install_rbspy.sh
@@ -0,0 +1,5 @@
+wget https://github.com/rbspy/rbspy/releases/download/v0.42.1/rbspy-x86_64-unknown-linux-gnu.tar.gz
+tar -xzf rbspy-x86_64-unknown-linux-gnu.tar.gz
+mv rbspy-x86_64-unknown-linux-gnu rbspy
+rm rbspy-x86_64-unknown-linux-gnu*
+xmod +x rbspy
